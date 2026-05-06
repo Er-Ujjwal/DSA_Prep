1d0adc8c9c320db65ab427ac15582121f4fec179
Er-Ujjwal <theujjwal.r@gmail.com>
2026-05-07 05:14:42 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (41.61%) - LeetSync

--- Strings/2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
+++ Strings/2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
@@ -1,8 +1,10 @@
 class Solution {
 public:
     string removeOccurrences(string s, string part) {
-        while (s.length() >0 && s.find(part) < s.length()){
-            s.erase(s.find(part), part.length());
+        int pos = s.find(part);
+        while (s.length() >0 && pos < s.length()){
+            s.erase(pos, part.length());
+            pos = s.find(part);
         }
         return s;
     }
