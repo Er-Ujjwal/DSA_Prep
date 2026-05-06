85bd7549f8971e8c6a487e3df2043c560ba8ad1f
Er-Ujjwal <theujjwal.r@gmail.com>
2026-05-07 04:27:14 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (93.66%) - LeetSync

---
+++ Strings/125-valid-palindrome/valid-palindrome.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isAlphaNumeric(char ch){
+        if ((ch >=48 && ch <=57) || (ch >=65 && ch <=90) || (ch >=97 && ch<=122)){
+            return true;
+        }
+        return false;
+    }
+    bool isPalindrome(string s) {
+        int start = 0, end = s.size() - 1;
+        if (s.size() == 1 || s.size() == 0) return true;
+        while (start < end){
+            if (!isAlphaNumeric(s[start])){
+                start++; 
+                continue;
+            }
+            if (!isAlphaNumeric(s[end])){
+                end--;
+                continue;
+            } 
+            if (tolower(s[start++]) != tolower(s[end--])) return false;
+        }
+        return true;
+    }
+};
