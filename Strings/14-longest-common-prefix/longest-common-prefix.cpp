05c2339baf51077e1ed86e186d24b9bbb1ba7fc7
Er-Ujjwal <theujjwal.r@gmail.com>
2026-05-18 04:39:37 +0530

    Time: 2 ms (10.78%) | Memory: 11.8 MB (80.80%) - LeetSync

--- Strings/14-longest-common-prefix/longest-common-prefix.cpp
+++ Strings/14-longest-common-prefix/longest-common-prefix.cpp
@@ -3,7 +3,7 @@ public:
     string longestCommonPrefix(vector<string>& strs) {
         string res = "";
         for (int i=0; i<strs[0].size(); i++){
-            for (auto s : strs){
+            for (auto &s : strs){
                 if (i==s.size() || s[i] != strs[0][i]) return res;
             }
             res += strs[0][i];
