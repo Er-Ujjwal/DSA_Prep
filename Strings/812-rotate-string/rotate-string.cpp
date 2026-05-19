caba74c2507ff1b4e5c69e556f6da1de1808ff48
Er-Ujjwal <theujjwal.r@gmail.com>
2026-05-20 02:08:02 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (96.59%) - LeetSync

--- Strings/812-rotate-string/rotate-string.cpp
+++ Strings/812-rotate-string/rotate-string.cpp
@@ -3,7 +3,7 @@ public:
     bool rotateString(string s, string goal) {
         if (s.size() != goal.size()) return false;
         for (int i=0; i<s.size(); i++){
-            rotate(s.begin(), s.begin()+1, s.end());
+            rotate(begin(s), begin(s)+1, end(s));
             if (s==goal) return true;
         }
         return false;
