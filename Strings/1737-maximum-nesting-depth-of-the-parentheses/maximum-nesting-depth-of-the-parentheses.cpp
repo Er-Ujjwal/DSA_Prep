1359d1e4e15b2e37b807508b5787a2260ff57360
Er-Ujjwal <theujjwal.r@gmail.com>
2026-06-12 02:59:39 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (59.20%) - LeetSync

--- Strings/1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.cpp
+++ Strings/1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     int maxDepth(string s) {
         int depth = 0, ans = 0;
-        for (int i = 0; i < s.length(); i++) {
-            if (s[i] == '('){
+        for (char c : s) {
+            if (c == '('){
                 depth++;
                 ans = max(ans, depth);
             }
-            else if (s[i] == ')') depth--;
+            else if (c == ')') depth--;
         }
         return ans;
     }
