5ebb907c97f2d8506318e096f03ab59c3d0eb876
Er-Ujjwal <theujjwal.r@gmail.com>
2026-06-16 02:15:45 +0530

    Time: 115 ms (50.05%) | Memory: 8.9 MB (92.60%) - LeetSync

---
+++ Strings/1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int beautySum(string s) {
+        int ans = 0;
+        for (int i=0; i<s.size(); i++){
+            int freq[26] = {0};
+            for (int j=i; j<s.size(); j++){
+                freq[s[j]-'a']++;
+                int mx = 0;
+                int mn = INT_MAX;
+                for (int k=0; k<26; k++){
+                    if (freq[k] == 0) continue;
+                    mx = max(mx, freq[k]);
+                    mn = min(mn, freq[k]);
+                }
+                ans += mx - mn;
+            }
+        }
+        return ans;
+    }
+};
