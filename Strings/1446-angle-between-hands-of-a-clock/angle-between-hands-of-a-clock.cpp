ca1b36396da22bbb094c02ca6a84975c6a3e8ddb
Er-Ujjwal <theujjwal.r@gmail.com>
2026-06-19 04:33:21 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (10.51%) - LeetSync

--- Strings/1446-angle-between-hands-of-a-clock/angle-between-hands-of-a-clock.cpp
+++ Strings/1446-angle-between-hands-of-a-clock/angle-between-hands-of-a-clock.cpp
@@ -2,9 +2,12 @@ class Solution {
 public:
     double angleClock(int hour, int minutes) {
         //Minute hand angle from 12 = 6deg*M -> 360deg in 60 min = 6deg/minute;
-        //Hour hand angle from 12 = 0.5deg*H -> 360deg in 12hr = 30deg/hr = 0.5deg/mn;
+        //Hour hand angle from 12 = 0.5deg*H -> 360deg in 12hr = 30deg/hr =0.5deg/mn;
+        //For neglecting -ve value, taking absolute value here -> abs();
         hour%= 12;
-        double angle = abs(30*hour - 5.5*minutes);
-        return min(angle, 360-angle);
+        double minuteAngle = 6.0 * minutes;
+        double hourAngle = 30.0 * hour + 0.5 * minutes;
+        double angle = abs(hourAngle - minuteAngle);
+        return min(angle, 360.0 - angle);
     }
 };
