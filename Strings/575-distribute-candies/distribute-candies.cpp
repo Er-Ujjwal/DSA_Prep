b86c23a322065a2b7f7e7da1e1c0646d6cb67e14
Er-Ujjwal <theujjwal.r@gmail.com>
2026-06-22 04:03:06 +0530

    Time: 117 ms (36.66%) | Memory: 120.2 MB (40.81%) - LeetSync

--- Strings/575-distribute-candies/distribute-candies.cpp
+++ Strings/575-distribute-candies/distribute-candies.cpp
@@ -2,9 +2,7 @@ class Solution {
 public:
     int distributeCandies(vector<int>& candyType) {
         unordered_set<int> st(candyType.begin(), candyType.end());
-        int n = candyType.size();
-        int total = st.size();
-        return min(total, n/2);
+        return min((int)st.size(), (int)(candyType.size())/2);
         //Unordered_map can also be used but no use of frequemcy so set is better.
         //Initialization of st to directly candyType saves Runtime by 40%.
     }
