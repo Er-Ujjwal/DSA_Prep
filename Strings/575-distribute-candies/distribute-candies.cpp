0cfffacbfa274adbe42e21aa924864f9ce702515
Er-Ujjwal <theujjwal.r@gmail.com>
2026-06-22 04:02:04 +0530

    Time: 95 ms (73.54%) | Memory: 120.1 MB (40.81%) - LeetSync

--- Strings/575-distribute-candies/distribute-candies.cpp
+++ Strings/575-distribute-candies/distribute-candies.cpp
@@ -5,5 +5,7 @@ public:
         int n = candyType.size();
         int total = st.size();
         return min(total, n/2);
+        //Unordered_map can also be used but no use of frequemcy so set is better.
+        //Initialization of st to directly candyType saves Runtime by 40%.
     }
 };
