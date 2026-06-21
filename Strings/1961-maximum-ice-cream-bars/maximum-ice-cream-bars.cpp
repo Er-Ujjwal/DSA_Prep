28a35f34f66acfe1d5e6e8853dd62bfbe88c1c80
Er-Ujjwal <theujjwal.r@gmail.com>
2026-06-22 01:36:32 +0530

    Time: 34 ms (53.78%) | Memory: 80.2 MB (89.49%) - LeetSync

--- Strings/1961-maximum-ice-cream-bars/maximum-ice-cream-bars.cpp
+++ Strings/1961-maximum-ice-cream-bars/maximum-ice-cream-bars.cpp
@@ -1,10 +1,13 @@
 class Solution {
 public:
     int maxIceCream(vector<int>& costs, int coins) {
+        // Sorting helps to pick cheapest first.
         sort(costs.begin(), costs.end());
         int count = 0;
         for (int i=0; i<costs.size(); i++){
+            //If cost > coin then we can't buy, so break....
             if (costs[i] > coins) break;
+            // otherwise coins get reduced & icecream prchased..
             coins -= costs[i];
             count++;
         }
