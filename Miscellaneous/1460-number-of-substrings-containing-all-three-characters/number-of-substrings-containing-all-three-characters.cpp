34a8cc6ade98416db5e6f4e3f47aaa2c2836bd8a
Er-Ujjwal <theujjwal.r@gmail.com>
2026-07-29 02:52:33 +0530

    Move duplicate 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp to Miscellaneous

---
+++ Miscellaneous/1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int numberOfSubstrings(string s) {
+        vector<int> count(3,0);
+        int left = 0, ans = 0;
+        int n = s.size();
+        for (int right=0; right<n; right++){
+            count[s[right] - 'a']++;
+            
+            while (count[0] >0 && count[1] >0 && count[2] >0){
+                ans += (n-right);
+                count[s[left] - 'a']--;
+                left++;
+            }
+        }
+        return ans;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
