4eaf030fbc08d8ef561a7d0387faa50bb72ffdca
Er-Ujjwal <theujjwal.r@gmail.com>
2026-07-29 02:52:06 +0530

    Move 605-can-place-flowers/can-place-flowers.cpp to Greedy Algorithm

---
+++ Greedy Algorithm/605-can-place-flowers/can-place-flowers.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        int len = flowerbed.size();
+
+        for (int i = 0; i < len && n > 0; i++) {
+
+            bool left  = (i == 0) || (flowerbed[i-1] == 0);
+            bool right = (i == len-1) || (flowerbed[i+1] == 0);
+
+            if (flowerbed[i] == 0 && left && right) {
+                flowerbed[i] = 1;
+                n--;
+            }
+        }
+
+        return n == 0;
+    }
+};
