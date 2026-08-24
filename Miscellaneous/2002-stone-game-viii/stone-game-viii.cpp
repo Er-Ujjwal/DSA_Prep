7eac366254e23330ab40fa85e66889a185142489
Ujjwal Rauniyar <theujjwal.r@gmail.com>
2026-08-25 05:16:01 +0530

    Time: 96 ms (86.23%) | Memory: 87.1 MB (96.09%) - LeetSync

---
+++ Miscellaneous/2002-stone-game-viii/stone-game-viii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int stoneGameVIII(vector<int>& stones) {
+        int n = stones.size();
+        for (int i=1; i<n; i++){
+            stones[i] += stones[i-1];
+        }
+        int ans = stones[n-1];
+        for (int i=n-2; i>=1; i--){
+            ans = max(ans, stones[i] - ans);
+        }
+        return ans;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
