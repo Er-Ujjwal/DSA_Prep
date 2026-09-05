3d4d5d3a31013a0e147b533eb755e30ba179037e
Ujjwal Rauniyar <theujjwal.r@gmail.com>
2026-09-06 04:36:36 +0530

    Time: 0 ms (100.00%) | Memory: 202.7 MB (66.09%) - LeetSync

---
+++ Miscellaneous/4285-smallest-stable-index-ii/smallest-stable-index-ii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int firstStableIndex(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<int>suffixMin(n);
+        suffixMin[n-1] = nums[n-1];
+        for (int i=n-2; i>=0; i--){
+            suffixMin[i] = min(nums[i], suffixMin[i+1]);
+        }
+        int prefixMax = 0;
+        for (int i=0; i<n; i++){
+            prefixMax = max(nums[i], prefixMax);
+            if (prefixMax - suffixMin[i] <= k) return i;
+        }
+        return -1;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
